af1b5db7211bff8cfdf39eeac8f9de1dde4c1957
joway <joway.w@gmail.com>
2016-05-17 13:36:04 +0800

    Site updated: 2016-05-17 13:36:03

--- js/levenshtein.js
+++ js/levenshtein.js
@@ -1,1 +1,106 @@
-!function(t,n){"function"==typeof define&&"object"==typeof define.amd&&define.amd?define(function(){return n(t)}):"object"==typeof module&&module&&module.exports?module.exports=n(t):t.Levenshtein=n(t)}(this,function(t){function n(t,n){var e,i;for(e=-1,i=t.length;++e<i;)n(t[e],e,t)}function e(t,e){var i;return i=Array(t.length),n(t,function(t,n,r){i[n]=e(t,n,r)}),i}function i(t,e,i){return n(t,function(t,n,r){i=e(t,n,r)}),i}function r(t,e){var i,r,o;return o=this._matrix=[],t==e?this.distance=0:""==t?this.distance=e.length:""==e?this.distance=t.length:(i=[0],n(t,function(t,n){n++,i[n]=n}),o[0]=i,n(e,function(u,c){r=[++c],n(t,function(n,o){o++,t.charAt(o-1)==e.charAt(c-1)?r[o]=i[o-1]:r[o]=Math.min(i[o]+1,r[o-1]+1,i[o-1]+1)}),i=r,o[o.length]=i}),this.distance=r[r.length-1])}return r.prototype.toString=r.prototype.inspect=function(t){var n,r,o,u,c;for(n=this.getMatrix(),r=i(n,function(t,n){return Math.max(t,i(n,Math.max,0))},0),o=Array((r+"").length).join(" "),u=[];u.length<(n[0]&&n[0].length||0);)u[u.length]=Array(o.length+1).join("-");return u=u.join("-+")+"-",c=e(n,function(t){var n;return n=e(t,function(t){return(o+t).slice(-o.length)}),n.join(" |")+" "}),c.join("\n"+u+"\n")},r.prototype.getMatrix=function(){return this._matrix.slice()},r.prototype.valueOf=function(){return this.distance},r});
+(function(root, factory){
+  if (typeof define == 'function' && typeof define.amd == 'object' && define.amd) {
+    define(function(){
+      return factory(root);
+    });
+  } else if (typeof module == 'object' && module && module.exports) {
+    module.exports = factory(root);
+  } else {
+    root.Levenshtein = factory(root);
+  }
+}(this, function(root){
+
+  function forEach( array, fn ) { var i, length
+    i = -1
+    length = array.length
+    while ( ++i < length )
+      fn( array[ i ], i, array )
+  }
+
+  function map( array, fn ) { var result
+    result = Array( array.length )
+    forEach( array, function ( val, i, array ) {
+      result[i] = fn( val, i, array )
+    })
+    return result
+  }
+
+  function reduce( array, fn, accumulator ) {
+    forEach( array, function( val, i, array ) {
+      accumulator = fn( val, i, array )
+    })
+    return accumulator
+  }
+
+  // Levenshtein distance
+  function Levenshtein( str_m, str_n ) { var previous, current, matrix
+    // Constructor
+    matrix = this._matrix = []
+
+    // Sanity checks
+    if ( str_m == str_n )
+      return this.distance = 0
+    else if ( str_m == '' )
+      return this.distance = str_n.length
+    else if ( str_n == '' )
+      return this.distance = str_m.length
+    else {
+      // Danger Will Robinson
+      previous = [ 0 ]
+      forEach( str_m, function( v, i ) { i++, previous[ i ] = i } )
+
+      matrix[0] = previous
+      forEach( str_n, function( n_val, n_idx ) {
+        current = [ ++n_idx ]
+        forEach( str_m, function( m_val, m_idx ) {
+          m_idx++
+          if ( str_m.charAt( m_idx - 1 ) == str_n.charAt( n_idx - 1 ) )
+            current[ m_idx ] = previous[ m_idx - 1 ]
+          else
+            current[ m_idx ] = Math.min
+              ( previous[ m_idx ]     + 1   // Deletion
+              , current[  m_idx - 1 ] + 1   // Insertion
+              , previous[ m_idx - 1 ] + 1   // Subtraction
+              )
+        })
+        previous = current
+        matrix[ matrix.length ] = previous
+      })
+
+      return this.distance = current[ current.length - 1 ]
+    }
+  }
+
+  Levenshtein.prototype.toString = Levenshtein.prototype.inspect = function inspect ( no_print ) { var matrix, max, buff, sep, rows
+    matrix = this.getMatrix()
+    max = reduce( matrix,function( m, o ) {
+      return Math.max( m, reduce( o, Math.max, 0 ) )
+    }, 0 )
+    buff = Array( ( max + '' ).length ).join( ' ' )
+
+    sep = []
+    while ( sep.length < (matrix[0] && matrix[0].length || 0) )
+      sep[ sep.length ] = Array( buff.length + 1 ).join( '-' )
+    sep = sep.join( '-+' ) + '-'
+
+    rows = map( matrix, function( row ) { var cells
+      cells = map( row, function( cell ) {
+        return ( buff + cell ).slice( - buff.length )
+      })
+      return cells.join( ' |' ) + ' '
+    })
+
+    return rows.join( "\n" + sep + "\n" )
+  }
+
+  Levenshtein.prototype.getMatrix = function () {
+    return this._matrix.slice()
+  }
+
+  Levenshtein.prototype.valueOf = function() {
+    return this.distance
+  }
+
+  return Levenshtein
+
+}));
